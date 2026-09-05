bf14f504ed4235e6e0ecc39ed21b7963e0cdb5e7
IEVN1001-23002524 <85929@alumnos.utleon.edu.mx.com>
2026-09-05 09:58:55 -0600

    Operaciones básicas 05-09-2026

---
+++ src/temasTS/01-tipos-bas.ts
@@ -0,0 +1,13 @@
+console.log("Hola mundo")
+var num1= 23
+let num2 = 25
+const num3 = 22
+
+let nombres:string
+let num:number
+let activo:boolean
+
+let suma:number=num1+num2+num3
+
+console.log("La suma es: ' + suma")
+
